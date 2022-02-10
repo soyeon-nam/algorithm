ad2bc62d631268860b564c6c1197a7a25d36da2b
soyeon-nam <soyeonlnam@gmail.com>
2022-02-10 17:42:05 +0900

    fix: dxdy_technique_8

--- codeTree/dxdy_technique/8_colored.py
+++ codeTree/dxdy_technique/8_colored.py
@@ -5,17 +5,22 @@ coor = [ [0] * n
     for _ in range(n)
 ]
 
-def checkAdjacent(x, y) :
-    ret = 0
+
+def inRange(x, y) :
+    return 0 <= x and x < n and 0 <= y and y < n
+
+
+def cntAdjacent(x, y) :
+    cnt = 0
     for i in range(4) :
         nx, ny = x + dx[i], y + dy[i]
-        if 0 <= nx and nx < n and 0 <= ny and ny < n :
-            ret += 1 if coor[nx][ny] == 1 else 0
-    return (True if ret >= 3 else False)
+        if inRange(nx, ny) and coor[nx][ny] == 1:
+            cnt += 1
+    return (cnt)
 
 
 for _ in range(m) :
     x, y = tuple(map(int, (input().split())))
     x, y = x - 1, y - 1
     coor[x][y] = 1
-    print(1 if checkAdjacent(x, y) == True else 0)
+    print(1 if cntAdjacent(x, y) == 3 else 0)
