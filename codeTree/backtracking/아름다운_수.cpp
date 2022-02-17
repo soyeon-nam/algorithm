3d792dc65bc54756b0bb0b0906d4217a931dbc9b
soyeon-nam <soyeonlnam@gmail.com>
2022-02-17 20:17:39 +0900

    add: backtracking_아름다운_수

---
+++ codeTree/backtracking/아름다운_수.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n;
+int res;
+vector<int> seq;
+
+bool IsBeautiful() {
+	for(int i = 0; i < n; i += seq[i]) {
+		if(i + seq[i] - 1 >= n)
+            return false;
+
+        for(int j = i; j < i + seq[i]; j++)
+            if(seq[j] != seq[i])
+                return false;
+	}
+	return true;
+}
+
+void CountBeautifulSeq(int cnt) {
+	if(cnt == n) {
+		if(IsBeautiful())
+			res++;
+		return;
+	}
+	
+	for(int i = 1; i <= 4; i++) {
+		seq.push_back(i);
+		CountBeautifulSeq(cnt + 1);
+		seq.pop_back();
+	}
+}
+
+int main() {
+	cin >> n;
+	
+	CountBeautifulSeq(0);
+	
+	cout << res;
+	return 0;
+}
